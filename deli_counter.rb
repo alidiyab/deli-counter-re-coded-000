6626d2467ec95ed3ee7ed3adff0c16f834460eb7
Ali Diyab <alidiyab1997@gmail.com>
2016-12-21 12:36:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,34 @@
 # Write your code here.
+
+# require 'pry'
 def line(katz_deli)
   if katz_deli == Array.new
     puts "The line is currently empty."
-  else
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  else str="The line is currently:"#1. Logan 2. Avi 3. Spencer
+   katz_deli.each_with_index do |name,index|
+    # binding.pry
+    str+=" #{index+1}. #{name}"
   end
+  puts str
+ end
 end
+# line([])
 
 def take_a_number(katz_deli, name)
   if katz_deli ==Array.new
     katz_deli.push(name)
-    katz_deli
     puts "Welcome, #{name}. You are number 1 in line."
   else
     katz_deli.push(name)
-    katz_deli
-    puts "Welcome, #{name}. You are number #{katz_deli.index(name) +1} in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   end
 end
 
 def now_serving(katz_deli)
-  if katz_deli ==Array.new
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.shift
-  puts "Currently serving Logan."
+  puts "Currently serving #{katz_deli.first}."
+  katz_deli.shift
   end
 end
